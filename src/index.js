ab77b6fd03267e00d8ef5a3df1d21bea7d8bb5b3
Gosia <77539154+codingGosia@users.noreply.github.com>
2022-09-22 09:09:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     img: 'src/images/fries.png',
   },
   { name: 'cheeseburger',
-    img: 'src/images/cheesburger.png',
+    img: 'src/images/cheeseburger.png',
   },
   { name: 'ice-cream',
     img: 'src/images/ice-cream.png',
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     img: 'src/images/fries.png',
   },
   { name: 'cheeseburger',
-    img: 'src/images/cheesburger.png',
+    img: 'src/images/cheeseburger.png',
   },
   { name: 'ice-cream',
     img: 'src/images/ice-cream.png',
@@ -44,23 +44,31 @@ console.log(cards)
 
 const grid = document.querySelector('.grid')
 let cardsChosen = []
+let cardsChosenIds = []
 
 function createBoard() {
   for (let i = 0; i < cards.length; i++){
     const card = document.createElement('img')
     card.setAttribute('src', 'src/images/blank.png')
     card.setAttribute('data-id', i)
-    cards.addEventListener('click', flipCard)
+    card.addEventListener('click', flipCard)
     grid.appendChild(card)
   }
 }
-createBoard()
 
 function flipCard() {
   let cardId = this.getAttribute('data-id')
-  cardsChosen.push(cards[cardId])
+  cardsChosen.push(cards[cardId].name)
+  cardsChosenIds.push(cardId)
+  this.setAttribute('src', cards[cardId].img)
+  if (cardsChosen.length === 2){
+    setTimeout(checkForMatch, 500)
+  }
+  }
 
-}
+
+  createBoard()
+
 
 
 })
